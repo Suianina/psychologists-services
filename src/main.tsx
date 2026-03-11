6b634f211ebe48f625b87c41b4d5eecf3cd28eb6
Zhanna Suianina <cezyby3@gmail.com>
2026-03-12 00:14:52 +0200

    add index.html, main.tsx and App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "modern-normalize/modern-normalize.css";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/psychologists-services">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
