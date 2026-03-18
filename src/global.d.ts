3b4b615378d160e2ce4b9a6b1558d02b27cf0472
Zhanna Suianina <cezyby3@gmail.com>
2026-03-18 16:09:20 +0200

    feat: add Firebase config files and TypeScript declarations

---
+++ src/global.d.ts
@@ -0,0 +1,49 @@
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.sass" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "modern-normalize/modern-normalize.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: string;
+  export default content;
+}
