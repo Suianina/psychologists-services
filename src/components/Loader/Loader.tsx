535882fa4a6d60391cd172ec103b427245eb3e05
Zhanna Suianina <cezyby3@gmail.com>
2026-03-19 12:39:12 +0200

    feat: add Loader component with spinner animation

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,3 +1,11 @@
-export default function Loader() {
-  return null;
-}
+import styles from "./Loader.module.css";
+
+const Loader = () => {
+    return (
+        <div className={styles.loaderContainer}>
+            <div className={styles.spinner}></div>
+        </div>
+    );
+};
+
+export default Loader;
